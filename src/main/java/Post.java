46d1b5013f6a190fac1437a331de9d02e712ce28
Yu V <valeriyyugay96@yandex.ru>
2023-06-06 13:16:29 +0300

    Initial Commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public String surname;
+    public String patronymic;
+    public boolean subscription;
+    public String passport;
+    public String phone;
+
+}
